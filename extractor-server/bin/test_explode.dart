1592b840ede718fce5549cc4b968b560783f5ce3
Akash kumar <akashkumar48874@gmail.com>
2026-08-26 01:46:53 +0530

    test: getManifest without watch page

--- extractor-server/bin/test_explode.dart
+++ extractor-server/bin/test_explode.dart
@@ -3,21 +3,18 @@ import 'dart:io';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 void main() async {
-  print('Running YouTubeExplode diagnostics...');
+  print('Running YouTubeExplode diagnostics without watch page...');
   final yt = YoutubeExplode();
   try {
-    final video = await yt.videos.get('kJQP7kiw5Fk');
-    print('Video Title: ${video.title}');
-    print('Author: ${video.author}');
-    
-    print('Fetching Stream Manifest...');
+    print('Fetching Stream Manifest with requireWatchPage: false & ytClients: [ios]...');
     final manifest = await yt.videos.streamsClient.getManifest(
       'kJQP7kiw5Fk',
       ytClients: [YoutubeApiClient.ios],
+      requireWatchPage: false,
     );
     print('Audio Streams count: ${manifest.audioOnly.length}');
     for (final s in manifest.audioOnly) {
-      print('Stream: ${s.tag} | ${s.container.name} | ${s.bitrate.bitsPerSecond} bps | url: ${s.url.toString().substring(0, 50)}...');
+      print('Stream: ${s.tag} | ${s.container.name} | ${s.bitrate.bitsPerSecond} bps | url: ${s.url.toString().substring(0, 60)}...');
     }
   } catch (e, st) {
     print('Error: $e');
